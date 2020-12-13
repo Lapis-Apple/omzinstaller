df4bddb700bc80b4d43e529026b84ac9002ee762
Lapis-Apple <62464927+Lapis-Apple@users.noreply.github.com>
2020-12-13 20:55:37 +0800

    Update omzinstaller.sh

--- omzinstaller.sh
+++ omzinstaller.sh
@@ -23,9 +23,9 @@ fi
 #从Fastgit得到oh-my-zsh安装脚本
 curl -o ohmyzshinstall.sh https://raw.fastgit.org/ohmyzsh/ohmyzsh/master/tools/install.sh
 #加上可执行权限
-chmod +x ./install.sh
+chmod +x ./ohmyzshinstall.sh
 #开始安装 （不运行zsh 安装在/usr/share/oh-my-zsh 从Fastgit下载）
-RUNZSH=no ZSH=${ZSH:-/usr/share/oh-my-zsh} REMOTE=${REMOTE:-https://hub.fastgit.org/${REPO}.git} ./install.sh
+RUNZSH=no ZSH=${ZSH:-/usr/share/oh-my-zsh} REMOTE=${REMOTE:-https://hub.fastgit.org/${REPO}.git} ./ohmyzshinstall.sh
 #备份template zshrc
 cp /usr/share/oh-my-zsh/templates/zshrc.zsh-template /usr/share/oh-my-zsh/templates/zshrc.zsh-template_bak
 #删掉安装脚本生成的zshrc
